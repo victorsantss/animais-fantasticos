b6d3cc1454e48c50272dfbc0aa4e369435b75347
Victor Santos <victor.santos@multi24h.com.br>
2021-09-30 20:47:42 -0300

    dev link ajustado

--- script.js
+++ script.js
@@ -60,19 +60,24 @@ function initScrollSuave() {
 }
 initScrollSuave();
 
-const sections = document.querySelectorAll('.js-scroll')
-const windowMetade = window.innerHeight * 0.8
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll('.js-scroll');
+  if(sections.length) {
+    const windowMetade = window.innerHeight * 0.8;
 
-function animaScroll() {
-  sections.forEach((section) => {
-    const sectionTop = section.getBoundingClientRect().top;
-    const isSectionVisible = (sectionTop - windowMetade) < 0
-    if(isSectionVisible) {
-      section.classList.add('ativo')
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = (sectionTop - windowMetade) < 0;
+        if(isSectionVisible)
+          section.classList.add('ativo');
+      })
     }
-  })
+
+    animaScroll();
+
+    window.addEventListener('scroll', animaScroll);
+  }
 }
+initAnimacaoScroll();
 
-animaScroll();
-
-window.addEventListener('scroll', animaScroll);
