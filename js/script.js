1998d2f19a26701e209258b500a3ed652803401a
Victor Santos <victords895@gmail.com>
2021-10-24 21:59:02 -0300

    add animacoes 3

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 import initAnimacaoScroll from "./modules/accordion.js";
+import initAnimaNumeros from "./modules/anima-numeros.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
+import initFuncionamento from "./modules/funcionamento.js";
+import initMenuMobile from "./modules/menu-mobile.js";
 import initModal from "./modules/modal.js";
 import initAccordion from "./modules/scroll-animacao.js";
 import initScrollSuave from "./modules/scroll-suave.js";
@@ -14,6 +17,9 @@ initTabNav();
 initModal();
 initTooltip();
 initDropdownMenu();
+initMenuMobile();
+initAnimaNumeros();
+initFuncionamento();
 
 
 
