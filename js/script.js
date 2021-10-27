aee2589f210b32af8234df0433f156760a5ba0a6
Victor Santos <victords895@gmail.com>
2021-10-26 22:54:56 -0300

    webpack & eslint add

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 import initAnimacaoScroll from "./modules/accordion.js";
-import initAnimaNumeros from "./modules/anima-numeros.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
+import initFetchAnimais from "./modules/fetch-animais.js";
+import initFetchBitcoin from "./modules/fetch-bitcoin.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initModal from "./modules/modal.js";
@@ -8,7 +9,6 @@ import initAccordion from "./modules/scroll-animacao.js";
 import initScrollSuave from "./modules/scroll-suave.js";
 import initTabNav from "./modules/tabnav.js";
 import initTooltip from "./modules/tooltip.js";
-
 
 initAccordion();
 initScrollSuave();
@@ -18,12 +18,6 @@ initModal();
 initTooltip();
 initDropdownMenu();
 initMenuMobile();
-initAnimaNumeros();
 initFuncionamento();
-
-
-
-
-
-
-
+initFetchAnimais();
+initFetchBitcoin();
